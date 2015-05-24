d2d30a03fe2eba2109ae4baae5ed429038bdf3bc
T. Takikawa <dromger@gmail.com>
2015-05-23 22:46:16 -0700

    Added crystal spinning and health mechanism
    The health mechanism uses RPCs

---
+++ Assets/Scripts/Crystal.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+public static var crystalDamage:float =  1000;
+var projectileDamage:int;
+
+var rotationDirectionValue:int = 1;
+var rotationDirection:Vector3;
+var rotationSpeed:double = 50;
+
+function Update(){
+	if (crystalDamage <= 0 || PlayerGUI.health <=0){
+		Application.LoadLevel("GameOver");
+	}
+	
+	switch (rotationDirectionValue){
+		case 0:
+		rotationDirection = Vector3.left;
+		break;
+		case 1:
+		rotationDirection = Vector3.right;
+		break;
+		case 2:
+		rotationDirection = Vector3.forward;
+		break;
+		case 3:
+		rotationDirection = Vector3.back;
+		break;
+	}
+	transform.Rotate(rotationDirection*Time.deltaTime*rotationSpeed);
+}
+
+function OnCollisionEnter(collision:Collision){
+	if (collision.gameObject.tag == "Projectile"){
+		crystalDamage -= projectileDamage;
+		SyncCrystal(crystalDamage);
+	}
+}
+
+@RPC
+function SyncCrystal(crystalNumber:int){
+	crystalDamage = crystalNumber;
+}
+//This is to ensure that players all have the same data
