8c2beec4d1f3bf18b03b3f5a97e1390bf7fb6636
T. Takikawa <dromger@gmail.com>
2015-05-23 23:16:22 -0700

    More network stuff I forgot to add

---
+++ Assets/Prefabs/PlayerPackage/NetworkInstantiate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NetworkInstantiate : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		if(GetComponent<NetworkView>().isMine) {/*
+			if(networkView.GetComponent<Animator>() != null)
+				networkView.GetComponent<Animator>().enabled = true;
+			if(networkView.GetComponent<Camera>() != null)
+				networkView.GetComponent<Camera>().enabled = true;*//*
+			if(networkView.GetComponent(Animator) != null)
+				networkView.GetComponen(Animator).enabled = true;*/
+				if(GetComponent<NetworkView>().GetComponentInChildren<Camera>() != null)
+					GetComponent<NetworkView>().GetComponentInChildren<Camera>().enabled = true;
+		}
+		else {
+			name += "Remote";
+		}
+	}
+	
+	//Update is called once per frame
+	void Update () {
+		//Debug.Log (gameObject.camera.GetComponent<Camera>().enabled);
+	}
+}
